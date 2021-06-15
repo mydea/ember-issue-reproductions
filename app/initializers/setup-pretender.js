1b05b0f43cacc071b0e59f6d8b5b4a3d019ad3e9
Francesco Novy <francesco@fabscale.com>
2021-06-15 08:30:11 +0200

    Install Pretender & add minimal usage

---
+++ app/initializers/setup-pretender.js
@@ -0,0 +1,11 @@
+import Pretender from 'pretender';
+
+export function initialize() {
+  new Pretender(function () {
+    // here the Pretender config would go
+  });
+}
+
+export default {
+  initialize,
+};
